0faa4374e287dd43b2b68b902f8c725924fedc19
theswolf <chris.geymo@gmail.com>
2013-06-07 13:55:00 +0200

    First commit

---
+++ Tableroids/src/com/september/tableroids/WalkingActivity.java
@@ -0,0 +1,20 @@
+package com.september.tableroids;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class WalkingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // requesting to turn the title OFF
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // making it full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // set our MainGamePanel as the View
+        setContentView(new MainGamePanel(this));
+    }
+}
